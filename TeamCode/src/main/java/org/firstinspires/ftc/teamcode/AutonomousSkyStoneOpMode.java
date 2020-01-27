1e3d5108cbe51892dc6ad9237aafb78a9e8f87b0
Team 8872 <teamrobotics8872@gmail.com>
2020-01-27 15:38:00 -0800

    add OpenCVSkyStoneDetector.java, AutonomousSkyStoneOpMode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousSkyStoneOpMode.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+public abstract class AutonomousSkyStoneOpMode extends SkyStoneOpMode {
+    @Override
+    protected void initialize(boolean calibrateIMU) {
+        super.initialize(true);
+    }
+}
