6893436d85f53f0bd37f8b01c520d4293e58f76f
Team 8872 <teamrobotics8872@gmail.com>
2020-03-06 16:44:45 -0800

    blue works(TODO: reduce timing, don't crash into bot at end)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousBlue.java
@@ -11,16 +11,24 @@ import java.util.Locale;
 @Config
 public class AutonomousBlue extends AutonomousSkyStoneOpMode {
 
-    public static double forward = 23;
-    public static double leftX = -10;
-    public static double midX = 5;
-    public static double rightX = 17;
+    public static double forward = 25;
+    public static double leftX = -3;
+    public static double midX = 6;
+    public static double rightX = 8;
+
+    public static double leftXCycle2 = 14;
+    public static double midXCycle2 = 23;
+    public static double rightXCycle2 = 29;
 
     public static double pY = 0.3;
     public static double pX = 0.2;
     public static double brakeDistanceY = 15;
     public static double brakeDistanceX = 10;
     public static double brakeVelocity = 0.3;
+
+    public static double buildingZoneX = -70;
+    public static double foundationY = 30;
+    public static double buildingZoneY = 21;
 
     @Override
     public void runOpMode() {
@@ -29,25 +37,37 @@ public class AutonomousBlue extends AutonomousSkyStoneOpMode {
         telemetry.update();
 
         waitForStart();
+        createGlobalPositionUpdate();
 
         // synchronously move the actuator
-        actuatormove(1, 700, () -> {
+        actuatormove(1, 600, () -> {
             actuator.setPower(0);
             reach.setPosition(0.25);
-            clamp.setPosition(0.1);
             spin.setPosition(0.2);
+            sleep(1000);
+            clamp.setPosition(0);
+            System.err.println("actuator move");
         });
 
-        createGlobalPositionUpdate();
         telemetry.addData("Skystone Position", skyStoneLocation);
         telemetry.update();
         phoneCam.stopStreaming();
+        double skyStoneXPosition;
         if (skyStoneLocation == SkyStoneLocation.LEFT) {
-            move(leftX, forward, pX, pY, brakeDistanceY, brakeDistanceX, brakeVelocity);
+            skyStoneXPosition = leftX;
+            move(leftX, forward - 7, pX, pY, brakeDistanceX, brakeDistanceY, brakeVelocity, 1500);
+            sleep(1000); // give servos time to move
+            move(leftX, forward, pX, 0.2, brakeDistanceX, brakeDistanceY, brakeVelocity, 2000);
         } else if (skyStoneLocation == SkyStoneLocation.MIDDLE) {
-            move(midX, forward, pX, pY, brakeDistanceY, brakeDistanceX, brakeVelocity);
+            skyStoneXPosition = midX;
+            move(midX, forward - 7, pX, pY, brakeDistanceX, brakeDistanceY, brakeVelocity, 1500);
+            sleep(1000); // give servos time to move
+            move(midX, forward, pX, 0.2, brakeDistanceX, brakeDistanceY, brakeVelocity, 2000);
         } else if (skyStoneLocation == SkyStoneLocation.RIGHT) {
-            move(rightX, forward, pX, pY, brakeDistanceY, brakeDistanceX, brakeVelocity);
+            skyStoneXPosition = rightX;
+            move(rightX, forward - 7, pX, pY, brakeDistanceX, brakeDistanceY, brakeVelocity, 1500);
+            sleep(1000); // give servos time to move
+            move(rightX, forward, pX, 0.2, brakeDistanceX, brakeDistanceY, brakeVelocity, 2000);
         } else {
             TextToSpeech tts = new TextToSpeech(hardwareMap.appContext, null);
             tts.setLanguage(Locale.US);
@@ -55,46 +75,75 @@ public class AutonomousBlue extends AutonomousSkyStoneOpMode {
             return;
         }
 
-        clamp.setPosition(0.62);
-        sleep(200);
+        clamp.setPosition(0.6); // grab block
+        sleep(2000);
         lift.setPosition(0);
-        sleep(300);
-        actuatormove(-1, 300);
-        move(leftX, 13, pX, pY, 10, brakeDistanceX, brakeVelocity); // move backward
+        sleep(2000);
+        actuatormove(-1, 500);
+        move(skyStoneXPosition, buildingZoneY, pX, pY, brakeDistanceX, 10, brakeVelocity, 10000); // move backward
 
-        move(-80, 13, 0.4, pY, brakeDistanceY, 5, brakeVelocity); // move to building zone
-        move(-80, forward + 10, pX, pY, brakeDistanceY, 5, brakeVelocity); // move to foundation
+        move(buildingZoneX, buildingZoneY, 0.4, pY, 5, brakeDistanceY, brakeVelocity, 5000); // move to building zone
+        move(buildingZoneX, foundationY, pX, 0.2, 5, brakeDistanceY, brakeVelocity, 2000); // move to foundation
 
+        clamp.setPosition(0);//drops block
+        lift.setPosition(0.58);
+        move(buildingZoneX, buildingZoneY, pX, pY, brakeDistanceX, 10, brakeVelocity, 1000); // move backward
+
+
+        // Second Skystone ------------------------------------------
+
+        if (skyStoneLocation == SkyStoneLocation.LEFT) {
+            move(leftXCycle2, buildingZoneY, pX, pY, brakeDistanceX, 10, brakeVelocity, 5000);
+            move(leftXCycle2, forward + 8, pX, 0.1, brakeDistanceX, brakeDistanceY, brakeVelocity, 2000);
+        } else if (skyStoneLocation == SkyStoneLocation.MIDDLE) {
+            move(midXCycle2, buildingZoneY, pX, pY, brakeDistanceX, 10, brakeVelocity, 5000);
+            move(midXCycle2, forward + 8, pX, 0.1, brakeDistanceX, brakeDistanceY, brakeVelocity, 2000);
+        } else if (skyStoneLocation == SkyStoneLocation.RIGHT) {
+            move(rightXCycle2, buildingZoneY, pX, pY, brakeDistanceX, 10, brakeVelocity, 5000);
+            move(rightXCycle2, forward + 8, pX, 0.1, brakeDistanceX, brakeDistanceY, brakeVelocity, 2000);
+        }
+
+        // grab block two
+        clamp.setPosition(0.6);
         sleep(1000);
+        lift.setPosition(0);
+        sleep(3000);
+
+        move(leftX, buildingZoneY, pX, pY,
+                brakeDistanceX, 10, brakeVelocity, 1000); // move backward
+
+        move(buildingZoneX, buildingZoneY, 0.4, pY, 5, brakeDistanceY, brakeVelocity, 5000); // move to building zone
+        move(buildingZoneX, 45, pX, 0.2, 5, brakeDistanceY, brakeVelocity, 2500); // move to foundation
+
+        clamp.setPosition(0);//drops block
+        lift.setPosition(0.58);
+        lift.setPosition(0);
+        sleep(1000);
+
+        reach.setPosition(0.45);
 
         foundationGrabberLeft.setPosition(1.0);
         foundationGrabberRight.setPosition(0.0);
-        sleep(2500);
 
-        leftFront.setPower(1);
-        leftRear.setPower(1);
-        rightFront.setPower(0.3);
-        rightRear.setPower(0.3);
-        sleep(2000);
-        brake();
+        sleep(1000);
+
+        move(buildingZoneX, -3, pX, 0.6, 5, brakeDistanceY, brakeVelocity, 2000); // back away from foundation
         foundationGrabberLeft.setPosition(0.0);
         foundationGrabberRight.setPosition(1.0);
-        clamp.setPosition(0.1);
-        lift.setPosition(0.6);
-        sleep(500);
-        lift.setPosition(0);
+        sleep(100);
+        move(-30, 5, 0.6, 0.6, 5, brakeDistanceY, brakeVelocity, 10000); // side drive away from foundation
 
-        telemetry.addData("X Position", () -> globalPositionUpdate.returnXCoordinate() / ODOMETER_COUNTS_PER_INCH);
-        telemetry.addData("Y Position", () -> globalPositionUpdate.returnYCoordinate() / ODOMETER_COUNTS_PER_INCH);
-        telemetry.addData("Orientation (Degrees)", () -> globalPositionUpdate.returnOrientation());
 
-        telemetry.addData("Vertical left encoder position", () -> rightRear.getCurrentPosition());
-        telemetry.addData("Vertical right encoder position", () -> leftFront.getCurrentPosition());
-        telemetry.addData("horizontal encoder position", () -> rightFront.getCurrentPosition());
+//        telemetry.addData("X Position", globalPositionUpdate.returnXCoordinate() / ODOMETER_COUNTS_PER_INCH);
+//        telemetry.addData("Y Position", globalPositionUpdate.returnYCoordinate() / ODOMETER_COUNTS_PER_INCH);
+//        telemetry.addData("Orientation (Degrees)", globalPositionUpdate.returnOrientation());
+//
+//        telemetry.addData("Vertical left encoder position", rightRear.getCurrentPosition());
+//        telemetry.addData("Vertical right encoder position", leftFront.getCurrentPosition());
+//        telemetry.addData("horizontal encoder position", rightFront.getCurrentPosition());
 
-        telemetry.update();
-        sleep(1000);
+//        telemetry.update();
+//        sleep(1000);
     }
-
 }
 
